01ff11d1cca304dadd4ee142c2387a072cee9231
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-02-11 20:18:00 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# 6
+n = int(input())
+for i in range(1, n + 1):
+    print(i)
+
+# 7
+n = int(input())
+s = 0
+for i in range(1, n + 1):
+    s += i
+print(s)
+
+# 8
+matn = input()
+print(matn.upper())
+
+# 9
+a = int(input())
+b = int(input())
+print("Teng" if a == b else "Teng emas")
+
+# 10
+son = int(input())
+print(str(son)[::-1])
